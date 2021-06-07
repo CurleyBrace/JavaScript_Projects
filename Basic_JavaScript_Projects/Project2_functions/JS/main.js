700d0e79e60b4412987f79bc49cc8eec65cf29b2
CurleyT <curleyt727@gmail.com>
2021-06-07 06:19:28 -0400

    Changes following Step 70 Adding Operators

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,6 +1,12 @@
-function varAssignVariables() {
+function varAssignVariables() { //Pulling two variables from HTML and changing them both on button click event
     var para = document.getElementById("varOne");
     var headerThree = document.getElementById("varTwo");
     para.style.color = "green";
     headerThree.style.color = "blue";
+}
+
+function plusEqualsFunction() {  //Two Variables being initialized and then concatenated using the += operator
+    var sentence = "This is the "; 
+    sentence += "plus/equals operator assignment.";
+    document.getElementById("plusEquals").innerHTML = sentence;
 }
